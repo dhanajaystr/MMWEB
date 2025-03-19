3e91c0affa936edcde0ba19682e16fd56f162c09
dhanajaystr <dhananjaymotghare511@gmail.com>
2025-03-19 03:38:03 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function openForm(plan) {
+    document.getElementById("form-popup").style.display = "block";
+    document.getElementById("selectedPlan").value = plan;
+}
+
+function closeForm() {
+    document.getElementById("form-popup").style.display = "none";
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Please wait, I’m calling back!");
+    closeForm();
+});
